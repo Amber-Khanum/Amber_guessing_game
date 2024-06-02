f05d1c1f5d225a0875368358cfcb4572cb7e2e5a
Amber <amberkhanum33@gmail.com>
2024-06-02 21:42:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number: ",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congrats! You guessed it right.");
+}
+else {
+    console.log("Sorry! You guessed it wrong.");
+}
+;
